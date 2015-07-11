1a55e2539f547cc1cbad744992482c302c60c55e
Ernesta Orlovaitė <ernesta@ernes7a.lt>
2015-07-11 21:30:59 +0100

    Initial commit.

---
+++ compute/blogs.py
@@ -0,0 +1,163 @@
+from __future__ import division
+import copy
+
+
+
+# CONSTANTS
+FILENAME = "blogs.txt"
+ITERATIONS = 10
+L = 0.8
+
+
+
+# FUNCTIONS
+### Utilities ###
+# Reads in a basic graph from a file.
+def readGraph(filename):
+	senders = {}
+	recipients = {}
+	
+	with open(filename, "rb") as file:
+		for line in file:
+			tokens = line.split("\t")
+			sender = tokens[0].strip()
+			recipient = tokens[1].strip()
+			
+			# Ignores links to itself
+			if sender == recipient:
+				continue
+					
+			# Adds sender and recipient to the graph
+			if sender not in senders:
+				senders[sender] = set()
+					
+			if recipient not in senders:
+				senders[recipient] = set()
+			
+			if sender not in recipients:
+				recipients[sender] = set()
+			
+			if recipient not in recipients:
+				recipients[recipient] = set()
+			
+			senders[sender].add(recipient)
+			recipients[recipient].add(sender)
+	
+	return senders, recipients
+
+
+# Normalises a dictionary of scores using a provided normaliser.
+def normalize(scores, norm):
+	for node, score in scores.iteritems():
+		scores[node] = score / norm
+
+
+### HITS ###
+# Initialises HITS values.
+def setupHITS(senderGraph):
+	initScore = 1.0 / pow(len(senderGraph), 0.5)
+	
+	hubs = {}
+	authorities = {}
+
+	for sender, recipients in senderGraph.iteritems():
+		hubs[sender] = initScore
+		authorities[sender] = initScore
+
+	return hubs, authorities
+
+
+# Iteratively computes hub and authority scores.
+def computeHITS(senderGraph, recipientGraph):
+	# Sets up data structures for HITS analysis
+	hubs, authorities = setupHITS(senderGraph)
+
+	for i in range(ITERATIONS):
+		norm = 0
+		
+		# Computes hub scores
+		for sender, recipients in senderGraph.iteritems():
+			hubs[sender] = 0
+			
+			for recipient in recipients:
+				hubs[sender] += authorities[recipient]
+	
+			norm += pow(hubs[sender], 2)
+		
+		# Normalizes hub scores
+		norm = pow(norm, 0.5)
+		normalize(hubs, norm)
+
+		# Computes authority scores
+		norm = 0
+		
+		for recipient, senders in recipientGraph.iteritems():
+			authorities[recipient] = 0
+			
+			for sender in senders:
+				authorities[recipient] += hubs[sender]
+		
+			norm += pow(authorities[recipient], 2)
+
+		# Normalizes authority scores
+		norm = pow(norm, 0.5)
+		normalize(authorities, norm)
+
+	return hubs, authorities
+
+
+### PageRank ###
+# Initialises PageRank values, creates a set of sinks.
+def setupPR(senderGraph):
+	initPR = 1.0 / len(senderGraph)
+	
+	currentPR = {}
+	sinks = set()
+	
+	for sender, recipients in senderGraph.iteritems():
+		currentPR[sender] = initPR
+		
+		if len(recipients) == 0:
+			sinks.add(sender)
+
+	return currentPR, sinks
+
+
+# Iteratively computes PageRank for each node.
+def computePR(senderGraph, recipientGraph):
+	N = len(recipientGraph)
+
+	# Sets up data structures for PageRank analysis.
+	currentPR, sinks = setupPR(senderGraph)
+	nextPR = {}
+
+	# Iterates pageRank computation ITERATIONS times.
+	for i in range(ITERATIONS):
+		# Sums PageRank over sinks
+		sinkSum = 0
+		for sink in sinks:
+			sinkSum += currentPR[sink]
+
+		# Goes through each node in the graph.
+		# Computes the sum of PageRank coming from each sender.
+		for recipient, senders in recipientGraph.iteritems():
+			incomingPR = 0
+		
+			for sender in senders:
+				outLinks = len(senderGraph[sender])
+			
+				if outLinks > 0:
+					incomingPR += currentPR[sender] / outLinks
+
+			nextPR[recipient] = (1 - L + L * sinkSum) / N + L * incomingPR
+	
+		# Sets the newly computed PageRank scores as current PageRank.
+		currentPR = copy.deepcopy(nextPR)
+
+	return currentPR
+
+
+# FLOW
+senders, recipients = readGraph(FILENAME)
+#print computeHITS(senders, recipients)
+print computePR(senders, recipients)
---
+++ network.js
@@ -0,0 +1,123 @@
+(function($) {
+	// Constants
+	// Container
+	var WIDTH = 400;
+	var HEIGHT = 400;
+	// Node
+	var RADIUS = 8;
+	// Arrowhead
+	var MARKER = 8;
+	var OFFSET = 20;
+	// Transitions
+	var TIME = 500;
+	// Colors
+	var COLOR = d3.scale.category20();
+	// DOM
+	var CONTAINER = "#network";
+	
+	// Elements
+	var force = d3.layout.force()
+		.charge(-120)
+		.linkDistance(50)
+		.size([WIDTH, HEIGHT]);
+		
+	var svg = d3.select(CONTAINER).append("svg")
+		.attr("width", WIDTH)
+		.attr("height", HEIGHT);
+	
+	// Network
+	d3.json("blogs.json", function(error, graph) {
+		if (error) throw error;
+		
+		force
+			.nodes(graph.nodes)
+			.links(graph.links)
+			.start();
+		
+		var def = svg.append("svg:defs")
+		
+		var marker = def.selectAll("marker")
+		    .data(["end"])
+		    .enter().append("svg:marker")
+		    .attr("id", "end")
+		    .attr("viewBox", "0 -5 10 10")
+		    .attr("refX", OFFSET)
+		    .attr("refY", -1.5)
+		    .attr("markerWidth", MARKER)
+		    .attr("markerHeight", MARKER)
+		    .attr("orient", "auto")
+		    .append("svg:path")
+		    .attr("d", "M0,-5L10,0L0,5");
+		
+		var link = svg.append("svg:g").selectAll(".link")
+			.data(force.links())
+			.enter().append("path")
+			.attr("class", "link")
+			.style("stroke-width", function(d) { d.value; })
+			.attr("marker-end", "url(#end)");
+		
+		var node = svg.selectAll(".node")
+			.data(force.nodes())
+			.enter().append("g")
+			.attr("class", "node")
+			.on("mouseover", mouseover)
+			.on("mouseout", mouseout)
+			.call(force.drag);
+		
+		node.append("circle")
+			.attr("r", RADIUS)
+			.style("fill", function(d) {
+				return stringToColor(d.name);
+			})
+		
+		node.append("text")
+			.attr("dx", 12)
+			.attr("dy", ".35em")
+			.text(function(d) {
+				return d.name
+			});
+		
+		force.on("tick", function() {
+			link.attr("d", function(d) {
+				var dx = d.target.x - d.source.x;
+				var dy = d.target.y - d.source.y;
+				var dr = Math.sqrt(dx * dx + dy * dy);
+				
+				return "M" + 
+					d.source.x + "," + 
+					d.source.y + "A" + 
+					dr + "," + dr + " 0 0,1 " + 
+					d.target.x + "," + 
+					d.target.y;
+			    });
+			
+			node.attr("transform", function(d) {
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+		});
+	});
+	
+	
+	function stringToColor(value) {
+		var hash = "";
+		
+		for (var i = 0; i < value.length; i++) {
+			hash += value.charCodeAt(i);
+		}
+		
+		return COLOR(parseInt(hash));
+	}
+	
+	function mouseover() {
+		d3.select(this).select("circle").transition()
+			.duration(TIME)
+			.attr("r", 1.3 * RADIUS);
+	}
+	
+	function mouseout() {
+		d3.select(this).select("circle").transition()
+			.duration(TIME)
+			.attr("r", RADIUS);
+	}
+	
+})(jQuery)
